080339ba16580278f00e6cb85879fd8d6313326f
Subhro-ai <subhrajyoti.singha30@gmail.com>
2025-12-24 21:27:02 +0530

    20.valid-parentheses

---
+++ 20.valid-parentheses.java
@@ -0,0 +1,38 @@
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/*
+ * @lc app=leetcode id=20 lang=java
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        // boolean res = true;
+        if (s.length()%2 == 1) return false;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+             else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+
+        }
+        return stack.isEmpty();
+
+    }
+}
+// @lc code=end
+
