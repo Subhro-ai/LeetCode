a2c40e82585bfecd373570603329852349b70df7
Subhro-ai <subhrajyoti.singha30@gmail.com>
2026-02-20 22:26:07 +0530

    151.reverse-words-in-a-string

---
+++ 151.reverse-words-in-a-string.java
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=151 lang=java
+ *
+ * [151] Reverse Words in a String
+ */
+
+// @lc code=start
+
+import java.util.ArrayDeque;
+
+class Solution {
+    public String reverseWords(String s) {
+        Deque<String> stack = new ArrayDeque<>();
+        String word;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if( c == ' ' ) continue;
+            else {
+                
+            }
+        }
+    }
+}
+// @lc code=end
+
