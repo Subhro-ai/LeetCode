9487554c4cc6580e1f663878d0af640d747a4e92
Subhro-ai <subhrajyoti.singha30@gmail.com>
2026-04-30 20:22:33 +0530

    705.design-hash-set

---
+++ 705.design-hash-set.java
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=705 lang=java
+ *
+ * [705] Design HashSet
+ */
+
+// @lc code=start
+
+import java.util.ArrayList;
+import java.util.List;
+
+class MyHashSet {
+    private List<Integer> data;
+    public MyHashSet() {
+        data = new ArrayList<>();
+    }
+    
+    public void add(int key) {
+        if (!data.contains(key)) {
+            data.add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        data.remove(Integer.valueOf(key));
+    }
+    
+    public boolean contains(int key) {
+        return data.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+// @lc code=end
+
