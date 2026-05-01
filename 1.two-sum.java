81e29e7f21b0fbc2b9bb779e68d9e1839b7c7f80
Subhro-ai <subhrajyoti.singha30@gmail.com>
2026-05-02 01:08:07 +0530

    347.top-k-frequent-elements

--- 1.two-sum.java
+++ 1.two-sum.java
@@ -12,19 +12,17 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int x = nums[i];
-            int needed = target - x;
-            // System.out.println("needed : " + needed);
-            if(map.containsKey(x)) {
-                return new int[] {map.get(x), i};
-                
-            } else {
-                map.put(needed, i);
-                // System.out.println("Put : " + needed + ":" + i);
+            int needed = target - nums[i];
+            if(map.containsKey(nums[i])) {
+                return new int[]{i, map.get(nums[i])};
             }
+            else {
+                map.put(needed, i);
+            } 
         }
         return new int[]{};
     }
+        
 }
 // @lc code=end
 
