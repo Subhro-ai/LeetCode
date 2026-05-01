0a6eb3157bafdfef19c800f468a8217e9cf39db0
Subhro-ai <subhrajyoti.singha30@gmail.com>
2026-05-01 23:02:23 +0530

    912.sort-an-array

---
+++ 912.sort-an-array.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=912 lang=java
+ *
+ * [912] Sort an Array
+ */
+
+// @lc code=start
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    private void mergeSort(int[] nums, int l, int r) {
+        if (l >= r) return;
+
+        int mid = (l + r) / 2;
+
+        mergeSort(nums, l, mid);
+        mergeSort(nums, mid + 1, r);
+
+        merge(nums, l, mid, r); 
+    }
+
+    private void merge(int[] nums, int l, int mid, int r) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArr[i] = nums[l + i];
+        }
+
+        for (int j = 0; j < n2; j++) {
+            rightArr[j] = nums[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                nums[k++] = leftArr[i++];
+            } else {
+                nums[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < n1) {
+            nums[k++] = leftArr[i++];
+        }
+
+        while (j < n2) {
+            nums[k++] = rightArr[j++];
+        }
+    }
+
+}
+// @lc code=end
+
