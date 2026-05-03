a19933aff52fcd1263aff7222ad446eb08d915db
Subhro-ai <subhrajyoti.singha30@gmail.com>
2026-05-03 19:44:25 +0530

    680.valid-palindrome-ii

---
+++ 680.valid-palindrome-ii.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=680 lang=java
+ *
+ * [680] Valid Palindrome II
+ */
+
+// @lc code=start
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while(i < j) {
+            if(s.charAt(i) != s.charAt(j)) {
+                return checkPalindrome(s, i + 1, j) || checkPalindrome(s, i, j - 1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    private boolean checkPalindrome(String s, int i, int j) {
+        while(i < j) {
+            if(s.charAt(i++) != s.charAt(j--)) return false;
+        }
+        return true;
+    }
+}
+// @lc code=end
+
